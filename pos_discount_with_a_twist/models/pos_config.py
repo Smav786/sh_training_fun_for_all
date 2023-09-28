bb3ebdad36c1bb4ccd28c3174c50b9d35c771016
Smav786 <146316108+Smav786@users.noreply.github.com>
2023-09-28 12:08:11 +0530

    Update pos_config.py

--- pos_discount_with_a_twist/models/pos_config.py
+++ pos_discount_with_a_twist/models/pos_config.py
@@ -2,7 +2,7 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 from odoo import api, fields, models
-#same
+
 
 class PosConfig(models.Model):
     _inherit = 'pos.config'
